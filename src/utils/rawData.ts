27fe749a7c0d1133f3b4dd040e90e3b096cf4785
PitchforkAssistant <pitchforkassistant@gmail.com>
2025-04-13 03:15:57 +0300

    raw data tests

--- src/utils/rawData.ts
+++ src/utils/rawData.ts
@@ -1,6 +1,6 @@
 import * as protos from "@devvit/protos";
 import {UserAboutResponse} from "@devvit/protos/types/devvit/plugin/redditapi/users/users_msg.js";
-import {Devvit} from "@devvit/public-api";
+import {Devvit, GetSubredditUsersByTypeOptions, ListingFetchOptions} from "@devvit/public-api";
 
 export type RedditAPIPlugins = {
     NewModmail: protos.NewModmail;
@@ -19,40 +19,48 @@ export type RedditAPIPlugins = {
 
 export type ExtendedDevvit = typeof Devvit & {
     redditAPIPlugins: RedditAPIPlugins
+    modLogPlugin: protos.Modlog
+    schedulerPlugin: protos.Scheduler
+    kvStorePlugin: protos.KVStore
+    redisPlugin: protos.RedisAPI
+    mediaPlugin: protos.MediaService
+    settingsPlugin: protos.Settings
+    realtimePlugin: protos.Realtime
 };
 
 export function getExtendedDevvit (): ExtendedDevvit {
     return Devvit as ExtendedDevvit; // The Devvit object already has the extended properties, they are simply not reflected in the public type definition.
 }
 
-export async function getRawUserData (username: string, metadata: protos.Metadata): Promise<UserAboutResponse | undefined> {
-    try {
-        return getExtendedDevvit().redditAPIPlugins.Users.UserAbout({username}, metadata);
-    } catch {
-        return undefined;
-    }
+export async function getRawUserData (username: string, metadata: protos.Metadata): Promise<UserAboutResponse> {
+    return getExtendedDevvit().redditAPIPlugins.Users.UserAbout({username}, metadata);
 }
 
-export async function getRawListingData (postOrCommentIds: string[], metadata: protos.Metadata): Promise<protos.Listing | undefined> {
-    try {
-        return getExtendedDevvit().redditAPIPlugins.LinksAndComments.Info({subreddits: [], thingIds: postOrCommentIds}, metadata);
-    } catch {
-        return undefined;
-    }
+export async function getRawListingData (postOrCommentIds: string[], metadata: protos.Metadata): Promise<protos.Listing> {
+    return getExtendedDevvit().redditAPIPlugins.LinksAndComments.Info({subreddits: [], thingIds: postOrCommentIds}, metadata);
 }
 
-export async function getRawPostData (thingIds: string[], metadata: protos.Metadata): Promise<protos.Listing | undefined> {
+export async function getRawPostData (thingIds: string[], metadata: protos.Metadata): Promise<protos.Listing> {
     return getRawListingData(thingIds, metadata);
 }
 
-export async function getRawCommentData (thingIds: string[], metadata: protos.Metadata): Promise<protos.Listing | undefined> {
+export async function getRawCommentData (thingIds: string[], metadata: protos.Metadata): Promise<protos.Listing> {
     return getRawListingData(thingIds, metadata);
 }
 
-export async function getRawSubredditData (subredditName: string, metadata: protos.Metadata): Promise<protos.SubredditAboutResponse | undefined> {
-    try {
-        return getExtendedDevvit().redditAPIPlugins.Subreddits.SubredditAbout({subreddit: subredditName}, metadata);
-    } catch {
-        return undefined;
-    }
+export async function getRawSubredditData (subredditName: string, metadata: protos.Metadata): Promise<protos.SubredditAboutResponse> {
+    return getExtendedDevvit().redditAPIPlugins.Subreddits.SubredditAbout({subreddit: subredditName}, metadata);
+}
+
+export async function setVote (id: string, dir: 1 | 0 | -1, metadata: protos.Metadata): Promise<void> {
+    await getExtendedDevvit().redditAPIPlugins.LinksAndComments.Vote({id, dir}, metadata);
+}
+
+export async function getRawUserWhereData (userWhereRequest: GetSubredditUsersByTypeOptions, fetchOptions: ListingFetchOptions, metadata: protos.Metadata): Promise<protos.Listing> {
+    return getExtendedDevvit().redditAPIPlugins.Subreddits.AboutWhere({
+        where: userWhereRequest.type,
+        subreddit: userWhereRequest.subredditName,
+        user: userWhereRequest.username,
+        ...fetchOptions,
+    }, metadata);
 }

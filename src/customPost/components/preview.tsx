df1dc28d438d1e074c70eacb0661d57edc9f03d8
PitchforkAssistant <pitchforkassistant@gmail.com>
2025-04-13 04:52:50 +0300

    add height to preview

--- src/customPost/components/preview.tsx
+++ src/customPost/components/preview.tsx
@@ -1,7 +1,9 @@
 import {Devvit} from "@devvit/public-api";
 
 export const CustomPostPreview: JSX.Element = (
-    <vstack alignment="middle center" grow>
-        <text>Loading Custom Post...</text>
-    </vstack>
+    <blocks height="tall">
+        <vstack alignment="middle center" grow>
+            <text>Loading Custom Post...</text>
+        </vstack>
+    </blocks>
 );
